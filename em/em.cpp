630bef41df97e8cc97370669b68403c9f17c3549
He Hao <hehaoslj@sina.com>
2019-03-04 19:30:51 +0800

    lmlab init version

---
+++ em/em.cpp
@@ -0,0 +1,9 @@
+/** Copyright 2018, 2019 He Hao<hehaoslj@sina.com> */
+
+#include <stdio.h>
+
+extern "C" int add(int, int);
+int main() {
+  wprintf(L"execute manager %d\n", add(12, 23));
+  return 0;
+}
---
+++ em/em1.c
@@ -0,0 +1,3 @@
+/** Copyright 2018, 2019 He Hao<hehaoslj@sina.com> */
+
+int add(int a, int b) { return a + b; }
